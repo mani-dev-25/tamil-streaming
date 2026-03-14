60d6256822855fd48091de766dcfa51693b407f4
mani13qwe <mani12345qwert@gmail.com>
2026-03-14 10:49:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// Connect to MongoDB Atlas
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
+
+// Video Schema
+const videoSchema = new mongoose.Schema({
+title: String,
+url: String
+});
+
+const Video = mongoose.model("Video", videoSchema);
+
+// API Route to get videos
+app.get("/api/videos", async (req, res) => {
+const videos = await Video.find();
+res.json(videos);
+});
+
+// Start Server
+app.listen(process.env.PORT, () => {
+console.log("Server running on port " + process.env.PORT);
+});
